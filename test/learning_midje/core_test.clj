2c4a56d449b02fc6da3225eb4cb60d4ec325e9bc
emaphis <emaphis85@gmail.com>
2014-08-06 18:09:58 -0400

    create a new leiningen project

---
+++ test/learning_midje/core_test.clj
@@ -0,0 +1,7 @@
+(ns learning-midje.core-test
+  (:require [clojure.test :refer :all]
+            [learning-midje.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
